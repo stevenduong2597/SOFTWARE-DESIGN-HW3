269ddb4f90ae008f4fd67c7e371c266371b265fc
Duc Duong <65508238+stevenduong2597@users.noreply.github.com>
2023-03-24 20:56:47 -0500

    Add files via upload

---
+++ Users.js
@@ -0,0 +1,46 @@
+const users = [
+    {
+        id: 1,
+        name: 'John',
+        email: 'John@gmail.com',
+        address: '123 Main st.',
+        city: 'Houston',
+        state: 'TX',
+        zipcode: '12345',
+        username: 'John123',
+        password: '123',
+        cardNumber: '123456789',
+        cardExp: '11/27',
+        cvv: '312'
+    },
+    {
+        id: 2,
+        name: 'Paul',
+        email: 'Paul@gmail.com',
+        address: '122 Cullen st.',
+        city: 'Austin',
+        state: 'TX',
+        zipcode: '12745',
+        username: 'Paul23',
+        password: '222',
+        cardNumber: '123456789',
+        cardExp: '12/25',
+        cvv: '999'
+    },
+    {
+        id: 3,
+        name: 'Pete',
+        email: 'Pete@gmail.com',
+        address: '45 6th st.',
+        city: 'Boston',
+        state: 'MA',
+        zipcode: '1111',
+        username: 'Pete123',
+        password: '767',
+        cardNumber: '000456789',
+        cardExp: '01/24',
+        cvv: '666'
+    },
+]
+
+module.exports = users;
